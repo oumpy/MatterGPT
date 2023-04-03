d371b6af4f0ad2801b40a733fe850bcbc9ceb232
AtamaokaC <realatamaokac@gmail.com>
2023-04-03 15:38:44 +0900

    Add setup_logger().

--- mattergpt.py
+++ mattergpt.py
@@ -60,6 +60,28 @@ def parse_args():
 
     return args
 
+def setup_logger(name, log_level=logging.DEBUG, log_file=None):
+    # Set up the logger
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+
+    # Configure log output to the console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(log_level)
+    console_formatter = logging.Formatter('[%(asctime)s] %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    # Configure log output to a file (optional)
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(log_level)
+        file_formatter = logging.Formatter('[%(asctime)s] %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
 def create_logging_stream(logfile, flush_logs):
     if logfile:
         log_stream = open(logfile, 'a', encoding='utf-8', buffering=1 if flush_logs else -1)
